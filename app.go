391685b6e85f7695763b9551357a85dedba34781
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-04-02 13:01:49 -0300

    feat: create principal function

--- app.go
+++ app.go
@@ -1,17 +1,32 @@
 package main
 
 import (
-    "database/sql"
+	"database/sql"
+	"fmt"
+	"log"
 
-    "github.com/gorilla/mux"
-    _ "github.com/lib/pq"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 type App struct {
-    Router *mux.Router
-    DB *sql.DB
+	Router *mux.Router
+	DB     *sql.DB
 }
 
-func (a *App) Initialize(user, password, dname string){}
+func (a *App) Initialize(user, password, dbname string) {
+	connectionString :=
+		fmt.Sprintf(
+			"user=%s password=%s dbname=%s sslmode=disable",
+			user, password, dbname)
+
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+}
 
 func (a *App) Run(addr string) {}
